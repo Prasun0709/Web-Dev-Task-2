50a65f015c32ee20ae0381fb0cb97d46167234a0
Prasun0709 <98683612+Prasun0709@users.noreply.github.com>
2024-03-25 19:34:18 +0530

    Add files via upload

---
+++ Designed-2/script.js
@@ -0,0 +1,4 @@
+document.getElementById('download-btn').addEventListener('click', function() {
+    // Replace with your download functionality or link
+    alert('Downloading FitTrack app...');
+});
